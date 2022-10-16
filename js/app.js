c0a4a19db986e6bf832b940be0fe474207624963
Onosereme Emuemhonjie <eonosereme@gmail.com>
2022-10-16 03:29:00 +0100

    '20%'

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const navToggler = document.querySelector(".nav-toggler");
+const sideBar = document.querySelector(".sidebar");
+const closeSidebar = document.querySelector(".close");
+
+navToggler.addEventListener("click", function () {
+  sideBar.classList.toggle("show-sidebar");
+  document.body.style.background = "rgba(0, 0, 0, .1)";
+});
+
+closeSidebar.addEventListener("click", function () {
+  sideBar.classList.remove("show-sidebar");
+  document.body.style.background = "none";
+});
