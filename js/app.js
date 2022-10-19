606d697d9f53e9f4259c6ab7d14b800bbe3e0982
Onosereme Emuemhonjie <eonosereme@gmail.com>
2022-10-19 02:18:40 +0100

    'dj'

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 const navToggler = document.querySelector(".nav-toggler");
-const sideBar = document.querySelector(".sidebar");
+const sidebar = document.querySelector(".sidebar");
 const closeSidebar = document.querySelector(".close");
+const sidebarLinks = document.querySelectorAll(".sidebar-links a");
 
-navToggler.addEventListener("click", function () {
-  sideBar.classList.toggle("show-sidebar");
-  document.body.style.background = "rgba(0, 0, 0, .4)";
+navToggler.addEventListener("click", function (e) {
+  sidebar.classList.toggle("show-sidebar");
+  //   document.body.style.background = "rgba(0, 0, 0, .5)";
+  e.stopPropagation();
 });
 
-closeSidebar.addEventListener("click", function () {
-  sideBar.classList.remove("show-sidebar");
-  document.body.style.background = "yellow";
+function hideSidebar() {
+  sidebar.classList.remove("show-sidebar");
+  //   document.body.style.background = "#ffffff";
+}
+
+closeSidebar.addEventListener("click", hideSidebar);
+
+window.addEventListener("click", hideSidebar);
+
+sidebar.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+sidebarLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    hideSidebar();
+  });
 });
